c23dd6f5f2a4111f0830f42d73971eb33b5756d0
Yaroslav-Pakhomov <pahomstyle90@mail.ru>
2022-04-29 14:51:49 +0500

    creating a Job class, working with queues, debugging code

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -6,13 +6,8 @@ namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\User\StoreRequest;
-use App\Mail\User\PasswordMail;
-use App\Models\User;
-use Illuminate\Auth\Events\Registered;
+use App\Jobs\StoreUserJob;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Str;
 
 class StoreController extends Controller
 {
@@ -23,12 +18,7 @@ class StoreController extends Controller
     public function __invoke(StoreRequest $request): RedirectResponse
     {
         $data = $request->validated();
-        $password = Str::random(10);
-        $data['password'] = Hash::make($password);
-        // $data['password'] = Hash::make($data['password']);
-        $user = User::firstOrCreate(['email' => $data['email']], $data);
-        Mail::to($data['email'])->send(new PasswordMail($password));
-        event(new Registered($user));
+        StoreUserJob::dispatch($data);
 
         return redirect()->route('admin.user.index');
     }
---
+++ app/Jobs/StoreUserJob.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\User\PasswordMail;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
+
+// use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class StoreUserJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public array $data;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        //
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        $password = Str::random(10);
+        $this->data['password'] = Hash::make($password);
+        // $data['password'] = Hash::make($data['password']);
+        $user = User::firstOrCreate(['email' => $this->data['email']], $this->data);
+        Mail::to($this->data['email'])->send(new PasswordMail($password));
+        event(new Registered($user));
+    }
+}
